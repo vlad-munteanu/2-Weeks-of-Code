cb0105b01d9889c1a3b07e64dfc83fde81ba2661
vlad-munteanu <vladcmunteanu@gmail.com>
2019-02-27 00:27:05 -0500

    first commit

---
+++ RandomColorGenerator/RandomColorGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomColorGenerator
+//
+//  Created by Vlad Munteanu on 2/27/19.
+//  Copyright © 2019 Vlad Munteanu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
