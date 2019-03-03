f5f23cea99635819d51e14b2efbdd4a7d0daab76
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-03 02:01:18 -0500

    Added AlertView

--- SSHToMac/SSHToMac/Controller/MainVC.swift
+++ SSHToMac/SSHToMac/Controller/MainVC.swift
@@ -13,6 +13,11 @@ class MainVC: UIViewController, UITextFieldDelegate {
     //Main View
     let mainView = MainView()
     
+    unowned var settingsButton: UIButton {return mainView.settingsButton}
+    unowned var sendButton: UIButton {return mainView.sendCommandButton}
+    
+    
+    
     public override func loadView() {
         self.view = mainView
     }
@@ -21,6 +26,42 @@ class MainVC: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         mainView.cmdTextfield.delegate = self
+        
+        self.settingsButton.addTarget(self, action: #selector(setup), for: UIControl.Event.touchUpInside)
+        self.sendButton.addTarget(self, action: #selector(sendCommand), for: UIControl.Event.touchUpInside)
+    }
+    
+    //TODO: Fix alertView
+    @objc func setup() {
+        let alert = UIAlertController(title: "Login", message: "Need to login to SSH", preferredStyle: .alert)
+        let ipTF = UITextField()
+        let passTF = UITextField()
+        
+        alert.addTextField(configurationHandler: { (textField) in
+            ipTF.placeholder = "IP Address"
+        })
+        alert.addTextField(configurationHandler: { (textField) in
+            passTF.placeholder = "Enter Password"
+        })
+        
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { action in
+            switch action.style{
+            case .default:
+                print("default")
+                
+            case .cancel:
+                print("cancel")
+                
+            case .destructive:
+                print("destructive")
+                
+                
+            }}))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @objc func sendCommand() {
+        
     }
     
     //dismisses keyboard when user touches screen elsewhere
