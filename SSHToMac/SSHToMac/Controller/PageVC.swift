a3f75d5892ded49e0197add88a6d7372c8f9e114
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-03 01:19:14 -0500

    Fixed pageVC

--- SSHToMac/SSHToMac/Controller/PageVC.swift
+++ SSHToMac/SSHToMac/Controller/PageVC.swift
@@ -11,6 +11,15 @@ import UIKit
 
 class PageVC: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
     
+    //Bye bye ugly page transtion
+    init(transitionStyle style:
+        UIPageViewController.TransitionStyle, navigationOrientation: UIPageViewController.NavigationOrientation, options: [String : Any]? = nil) {
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     //Delegate Methods
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         if let viewControllerIndex = self.pages.firstIndex(of: viewController) {
@@ -63,7 +72,7 @@ class PageVC: UIPageViewController, UIPageViewControllerDataSource, UIPageViewCo
         
         // add the individual viewControllers to the pageViewController
         self.pages.append(page1)
-        //self.pages.append(page2)
+        self.pages.append(page2)
         setViewControllers([pages[initialPage]], direction: .forward, animated: false, completion: nil)
         
         // pageControl
--- SSHToMac/SSHToMac/Supporting Files/AppDelegate.swift
+++ SSHToMac/SSHToMac/Supporting Files/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         if let window = self.window {
-            window.rootViewController = MainVC()
+            window.rootViewController = PageVC(transitionStyle: .scroll, navigationOrientation: .horizontal)
             window.makeKeyAndVisible()
         }
         
