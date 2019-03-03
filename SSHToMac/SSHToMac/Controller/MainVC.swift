139c4aed2054586568741b71fe90ae6f07b6ee8b
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-03 02:44:37 -0500

    Added basic SSH functionality

--- SSHToMac/SSHToMac/Controller/MainVC.swift
+++ SSHToMac/SSHToMac/Controller/MainVC.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import SwiftSH
 
 class MainVC: UIViewController, UITextFieldDelegate {
     
     var longStringOfCommands = ""
     //Main View
     let mainView = MainView()
+    
+    var myUsername = ""
+    var myPassword = ""
+    var myHost = ""
     
     unowned var settingsButton: UIButton {return mainView.settingsButton}
     unowned var sendButton: UIButton {return mainView.sendCommandButton}
@@ -32,14 +37,19 @@ class MainVC: UIViewController, UITextFieldDelegate {
         self.sendButton.addTarget(self, action: #selector(sendCommand), for: UIControl.Event.touchUpInside)
     }
     
-    //TODO: Fix alertView
+    //TODO: Fix alertView to allow users to input credentials
     @objc func setup() {
         let alert = UIAlertController(title: "Login", message: "Need to login to SSH", preferredStyle: .alert)
         let ipTF = UITextField()
         let passTF = UITextField()
+        let hostTF = UITextField()
         
         alert.addTextField(configurationHandler: { (textField) in
-            ipTF.placeholder = "IP Address"
+            ipTF.placeholder = "Enter Username"
+            
+        })
+        alert.addTextField(configurationHandler: { (textField) in
+            hostTF.placeholder = "Enter Hostname"
         })
         alert.addTextField(configurationHandler: { (textField) in
             passTF.placeholder = "Enter Password"
@@ -49,6 +59,7 @@ class MainVC: UIViewController, UITextFieldDelegate {
             switch action.style{
             case .default:
                 print("default")
+                
                 
             case .cancel:
                 print("cancel")
@@ -66,11 +77,45 @@ class MainVC: UIViewController, UITextFieldDelegate {
         if mainView.cmdTextfield.text == "" {
             notValidCommand(alertMessage: "Command Not Valid")
         } else {
+            connect()
             let command = mainView.cmdTextfield.text!
             longStringOfCommands = longStringOfCommands + command
             mainView.commandLabel.text = longStringOfCommands
+            connect()
         }
         print("send command")
+    }
+    func connect() {
+        let shell = Shell(host: myHost, port: 22)
+        // ...
+        shell!.withCallback { (string: String?, error: String?) in
+            print("\(string ?? error!)")
+            }
+            .connect()
+            .authenticate(.byPassword(username: myUsername, password: myPassword))
+            .open { (error) in
+                if let error = error {
+                    print("\(error)")
+                }
+        }
+        // ...
+        shell!.write("Say 'yuh'") { (error) in
+            if let error = error {
+                print("error")
+                print("\(error)")
+            }
+        }
+        
+//        let command = Command(host: "localhost", port: 22)
+//        // ...
+//        command!.connect().authenticate(.byPassword(username: "username", password: "password")).execute(command) { (command, result: String?, error) in
+//                if let result = result {
+//                    print("\(result)")
+//                } else {
+//                    print("ERROR: \(error)")
+//                }
+//        }
+        
     }
     
     func notValidCommand(alertMessage: String) {
