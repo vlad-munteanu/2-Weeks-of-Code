33b69113a4f67c4add78c49e7f0262d59cef822a
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-04 00:54:35 -0500

    Set up MainView

--- PomodoroTechnique/PomodoroTechnique/View/MainTimerView.swift
+++ PomodoroTechnique/PomodoroTechnique/View/MainTimerView.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import SnapKit
+
+public class MainTimerView: UIView {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.frame = CGRect.zero
+        
+        intializeUI()
+        createConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    internal func intializeUI() {
+        
+    }
+    
+    internal func createConstraints() {
+        
+    }
+    
+    //Making everything black and white
+    
+    //Main Background Subview
+    public let backgroundView: UIView = {
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        return backgroundView
+    }()
+    
+}
