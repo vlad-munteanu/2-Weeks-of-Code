524ecbc619c51eea6cf8b2ce2584950bbc980d72
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-04 00:10:50 -0500

    Setup basic MVC framework. No storyboard!

--- PomodoroTechnique/PomodoroTechnique/Supporting Files/AppDelegate.swift
+++ PomodoroTechnique/PomodoroTechnique/Supporting Files/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = self.window {
+            window.rootViewController = MainVC()
+            window.makeKeyAndVisible()
+        }
         return true
     }
 
---
+++ PomodoroTechnique/PomodoroTechnique/View/MainTimerView.swift
@@ -0,0 +1,9 @@
+//
+//  MainTimerView.swift
+//  PomodoroTechnique
+//
+//  Created by Vlad Munteanu on 3/4/19.
+//  Copyright © 2019 Vlad Munteanu. All rights reserved.
+//
+
+import Foundation
