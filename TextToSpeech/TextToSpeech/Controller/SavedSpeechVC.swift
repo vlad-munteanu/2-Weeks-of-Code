8a3ff095d9e29424ef5c2b3d809fa441e6378e1f
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-06 00:18:05 -0500

    Added PageController

---
+++ TextToSpeech/TextToSpeech/Controller/SavedSpeechVC.swift
@@ -0,0 +1,19 @@
+//
+//  SavedSpeechVC.swift
+//  TextToSpeech
+//
+//  Created by Vlad Munteanu on 3/6/19.
+//  Copyright © 2019 Vlad Munteanu. All rights reserved.
+//
+
+import UIKit
+class SavedSpeechVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
