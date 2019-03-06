37ab63b8bcbd6cc1c35640b36f35b3120f30147e
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-06 12:46:43 -0500

    Added Floating Label for textfield

--- TextToSpeech/TextToSpeech/View/InputView.swift
+++ TextToSpeech/TextToSpeech/View/InputView.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import SnapKit
+import JVFloatLabeledTextField
 
 public class InputView: UIView {
     
@@ -26,12 +27,18 @@ public class InputView: UIView {
     
     internal func intializeUI() {
         addSubview(backgroundView)
+        addSubview(textTF)
     }
     
     internal func createConstraints(){
         backgroundView.snp.makeConstraints { make in
             make.height.equalToSuperview()
             make.width.equalToSuperview()
+        }
+        textTF.snp.makeConstraints{ make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().multipliedBy(0.6)
+            make.width.equalToSuperview().multipliedBy(0.95)
         }
     }
     
@@ -40,6 +47,15 @@ public class InputView: UIView {
         let backgroundView = UIView()
         backgroundView.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         return backgroundView
+    }()
+    
+    //Textfields
+    public let textTF: JVFloatLabeledTextField = {
+        let tf = JVFloatLabeledTextField()
+        tf.floatingLabel.text = "Text"
+        tf.floatingLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        tf.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        return tf
     }()
     
 }
