3341a48671e7144ff93cb535ee22b0c4e39c266f
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-07 22:26:28 -0500

    No storyboard

---
+++ DengsterV2.1/DengsterV2.1/MainView.swift
@@ -0,0 +1,15 @@
+//
+//  MainView.swift
+//  DengsterV2.1
+//
+//  Created by Vlad Munteanu on 3/7/19.
+//  Copyright © 2019 Vlad Munteanu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SpriteKit
+
+public class MainView: SKView {
+    
+}
