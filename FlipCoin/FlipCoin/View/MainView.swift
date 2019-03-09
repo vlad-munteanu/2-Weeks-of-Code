48455d82b5bf93bae492caef10dd65bf7bb73607
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-09 15:24:27 -0500

    Fixed view

--- FlipCoin/FlipCoin/View/MainView.swift
+++ FlipCoin/FlipCoin/View/MainView.swift
@@ -11,4 +11,33 @@ import UIKit
 
 public class MainView: UIView {
     
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.frame = CGRect.zero
+        
+        intializeUI()
+        createConstraints()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    internal func intializeUI() {
+        addSubview(backgroundView)
+        
+    }
+    
+    func createConstraints() {
+        
+    }
+    
+    
+    //Main Background Subview
+    public let backgroundView: UIView = {
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        return backgroundView
+    }()
+    
 }
