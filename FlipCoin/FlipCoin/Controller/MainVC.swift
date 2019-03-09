1636fc1d2a69c1aadd7fc6c27c267cc4e6d2128b
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-09 00:50:14 -0500

    Created basic project structure

---
+++ FlipCoin/FlipCoin/Controller/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlipCoin
+//
+//  Created by Vlad Munteanu on 3/9/19.
+//  Copyright © 2019 Vlad Munteanu. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FlipCoin/FlipCoin/View/MainView.swift
@@ -0,0 +1,14 @@
+//
+//  MainView.swift
+//  FlipCoin
+//
+//  Created by Vlad Munteanu on 3/9/19.
+//  Copyright © 2019 Vlad Munteanu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class MainView: UIView {
+    
+}
