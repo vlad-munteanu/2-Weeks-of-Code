ba1f6671ed5b8775bab9b2e3b7f6b1451a32467f
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-09 16:03:54 -0500

    Fixed animation

--- FlipCoin/FlipCoin/Controller/MainVC.swift
+++ FlipCoin/FlipCoin/Controller/MainVC.swift
@@ -10,9 +10,67 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    
+    var coinString = "Heads"
+    //View
+    let mainView = MainView()
+    
+    //Button
+    unowned var flipButton: UIButton {return mainView.flipButton}
+    
+    //Label
+    unowned var flipLabel: UILabel {return mainView.headsTailLabel}
+    
+    var imgListArray :NSMutableArray = []
+    
+    public override func loadView() {
+        self.view = mainView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.flipButton.addTarget(self, action: #selector(flipStuff), for: UIControl.Event.touchUpInside)
+        setupImage()
+    }
+    
+    func setupImage() {
+        for i in 1...11 {
+            var strImageName : String = "coin\(i)"
+            var image = UIImage(named:strImageName)
+            imgListArray.add(image)
+        }
+    }
+    
+    @objc func flipStuff() {
+        let headsTails = Int(arc4random_uniform(2))
+        
+        flipImage(headsOrTails: headsTails)
+        
+        
+    }
+    
+    func flipImage(headsOrTails: Int) {
+        
+        flipButton.imageView!.animationImages = imgListArray as! [UIImage]
+        flipButton.imageView!.animationDuration = 2.0
+        flipButton.imageView!.startAnimating()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {  self.flipButton.imageView!.stopAnimating()
+        }
+        
+        if headsOrTails == 1 {
+            coinString = "Heads"
+            flipButton.setImage(UIImage(named: "coin10"), for: .normal)
+            flipLabel.text = "Heads"
+        } else {
+            coinString = "Tails"
+            flipButton.imageView!.stopAnimating()
+            flipButton.setImage(UIImage(named: "coin5"), for: .normal)
+            flipLabel.text = "Tails"
+        }
+        
+        
     }
 
 
--- FlipCoin/FlipCoin/View/MainView.swift
+++ FlipCoin/FlipCoin/View/MainView.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
 public class MainView: UIView {
     
@@ -25,19 +26,62 @@ public class MainView: UIView {
     
     internal func intializeUI() {
         addSubview(backgroundView)
-        
+        addSubview(flipButton)
+        addSubview(headsTailLabel)
     }
     
     func createConstraints() {
-        
+        backgroundView.snp.makeConstraints{ make in
+            make.height.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        flipButton.snp.makeConstraints{ make in
+            make.height.equalToSuperview().multipliedBy(0.4)
+            make.width.equalTo(flipButton.snp_height)
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        headsTailLabel.snp.makeConstraints{ make in
+            make.height.equalToSuperview().multipliedBy(0.3)
+            make.width.equalToSuperview().multipliedBy(0.9)
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().multipliedBy(1.4)
+            
+        }
     }
-    
     
     //Main Background Subview
     public let backgroundView: UIView = {
         let backgroundView = UIView()
-        backgroundView.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        backgroundView.backgroundColor = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)
         return backgroundView
     }()
+    
+    //Button
+    public let flipButton: UIButton = {
+        let flipBttn = UIButton()
+        flipBttn.setImage(UIImage(named: "coin0"), for: .normal)
+        
+        return flipBttn
+    }()
+    
+    //Label
+    public let headsTailLabel: UILabel = {
+        let headsTailLbl = UILabel()
+        headsTailLbl.font = UIFont(name: "Futura", size: 48)
+        headsTailLbl.numberOfLines = 1
+        headsTailLbl.minimumScaleFactor = 0.5
+        headsTailLbl.shadowOffset = CGSize(width: -3, height: 2)
+        headsTailLbl.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        headsTailLbl.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        headsTailLbl.text = "Tap to flip"
+        
+        headsTailLbl.adjustsFontSizeToFitWidth = true
+        headsTailLbl.textAlignment = .center
+        headsTailLbl.translatesAutoresizingMaskIntoConstraints = false
+        return headsTailLbl
+        
+    }()
+    
     
 }
