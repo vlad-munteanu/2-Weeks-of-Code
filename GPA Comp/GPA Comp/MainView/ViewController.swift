23b0b44efe9ba09d5bf8e6214f39bc1e3f0246b1
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-11 17:44:55 -0400

    Added alert view for login

--- GPA Comp/GPA Comp/MainView/ViewController.swift
+++ GPA Comp/GPA Comp/MainView/ViewController.swift
@@ -17,7 +17,49 @@ class MainVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.value(forKey: "username") == nil {
+            showAlert()
+        }
+    }
+    
+    func showAlert() {
         
+        let alertController = UIAlertController(title: "Not Logged In", message: "Enter PS Username and Password to Login", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "OK", style: .default, handler: {
+            alert -> Void in
+            
+            let usernameTF = alertController.textFields![0] as UITextField
+            let passwordTF = alertController.textFields![1] as UITextField
+            
+            if usernameTF.text != "" || passwordTF.text != ""{
+                PowerschoolInfo.setUpAuth(username: usernameTF.text!, password: passwordTF.text!)
+            }else{
+               print("nil")
+            }
+            
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: {
+            (action : UIAlertAction!) -> Void in
+            
+        })
+        
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Enter Username"
+        }
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Enter Password"
+            textField.isSecureTextEntry = true
+        }
+        
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: nil)
     }
     
     
---
+++ GPA Comp/GPA Comp/Model/PowerschoolInfo.swift
@@ -0,0 +1,21 @@
+//
+//  PowerschoolInfo.swift
+//  GPA Comp
+//
+//  Created by Vlad Munteanu on 3/11/19.
+//  Copyright © 2019 Vlad Munteanu. All rights reserved.
+//
+
+import Foundation
+import WKZombie
+
+public class PowerschoolInfo {
+    
+    class func setUpAuth(username: String, password: String) {
+        
+    }
+    class func pullInfo() {
+        
+    }
+}
+
