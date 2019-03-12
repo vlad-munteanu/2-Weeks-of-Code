cf25f8e8ad52bdcc1c61b62d1ba248167cd0fefe
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-12 09:48:13 -0400

    Fixed animations

--- FlipCoinAW/FlipCoinAW WatchKit Extension/InterfaceController.swift
+++ FlipCoinAW/FlipCoinAW WatchKit Extension/InterfaceController.swift
@@ -8,23 +8,45 @@
 
 import WatchKit
 import Foundation
+import UIKit
 
 
 class InterfaceController: WKInterfaceController {
-
-    
-    var imgListArray :NSMutableArray = []
-    
     @IBOutlet var coinImage: WKInterfaceImage!
     
     
     @IBAction func flipPressed() {
+        let headsTails = Int(arc4random_uniform(2))
+        coinImage.setImageNamed("coin")
         
+        
+       // self.coinImage.setImageNamed("coin10")
+        
+        if headsTails == 1 {
+            coinImage.startAnimatingWithImages(in: NSMakeRange(0,11), duration: 2.0, repeatCount: 1)
+            
+        } else {
+           coinImage.startAnimatingWithImages(in: NSMakeRange(0,6), duration: 2.5, repeatCount: 1)
+        }
+        
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+//            self.coinImage.stopAnimating()
+//            if headsTails == 1 {
+//                self.coinImage.setImageNamed("coin10")
+//
+//            } else {
+//                self.coinImage.stopAnimating()
+//                 self.coinImage.setImageNamed("coin5")
+//            }
+//        }
     }
+    
+    
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
         // Configure interface objects here.
+        
     }
     
     override func willActivate() {
