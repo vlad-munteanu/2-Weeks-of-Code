2539e7bb5c55c686844bf53d2a4df8058f8f6f25
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-13 08:51:49 -0400

    Added mainView

---
+++ GraphTheory/GraphTheory/View/ARView.swift
@@ -0,0 +1,40 @@
+//
+//  ARView.swift
+//  GraphTheory
+//
+//  Created by Vlad Munteanu on 3/13/19.
+//  Copyright © 2019 Vlad Munteanu. All rights reserved.
+//
+
+import UIKit
+import ARKit
+import SnapKit
+
+class ARView: UIView {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.frame = CGRect.zero
+        
+        intializeUI()
+        createConstraints()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    internal func intializeUI() {
+       addSubview(mainScene)
+    }
+    
+    internal func createConstraints(){
+        mainScene.snp.makeConstraints{
+            make.height.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+    }
+    
+    public let mainScene = ARSKView()
+    
+    
+}
