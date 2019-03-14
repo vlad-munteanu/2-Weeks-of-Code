ec1984dc388e5591d0f9a7a7d3629c8a0398646b
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-14 01:51:27 -0400

    Added basic structure and launchscreen

--- PresidentTweetCounter/PresidentTweetCounter/Supporting Files/AppDelegate.swift
+++ PresidentTweetCounter/PresidentTweetCounter/Supporting Files/AppDelegate.swift
@@ -12,10 +12,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = MainVC()
+        self.window?.makeKeyAndVisible()
         return true
     }
 
