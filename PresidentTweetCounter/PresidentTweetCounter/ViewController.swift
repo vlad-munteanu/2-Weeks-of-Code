d6ff1f488d9476ae38089bd92a3a162921f9ed70
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-14 01:28:13 -0400

    Added Day 14 project

---
+++ PresidentTweetCounter/PresidentTweetCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PresidentTweetCounter
+//
+//  Created by Vlad Munteanu on 3/14/19.
+//  Copyright © 2019 Vlad Munteanu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
