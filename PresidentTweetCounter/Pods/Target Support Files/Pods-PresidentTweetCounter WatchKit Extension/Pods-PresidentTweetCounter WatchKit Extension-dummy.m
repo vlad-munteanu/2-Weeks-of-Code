001a8a70959a9a45a84cf1537e1e9fb1c6285337
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-14 01:43:31 -0400

    Added app icons

---
+++ PresidentTweetCounter/Pods/Target Support Files/Pods-PresidentTweetCounter WatchKit Extension/Pods-PresidentTweetCounter WatchKit Extension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PresidentTweetCounter_WatchKit_Extension : NSObject
+@end
+@implementation PodsDummy_Pods_PresidentTweetCounter_WatchKit_Extension
+@end
---
+++ PresidentTweetCounter/Pods/Target Support Files/Pods-PresidentTweetCounter/Pods-PresidentTweetCounter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PresidentTweetCounter : NSObject
+@end
+@implementation PodsDummy_Pods_PresidentTweetCounter
+@end
