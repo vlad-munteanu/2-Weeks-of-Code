7efe3f41449635f7feb116b7ece2e41b8f4e6d57
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-25 08:30:45 -0400

    removed mainstoryboard

---
+++ SafePickup/SafePickup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafePickup
+//
+//  Created by Vlad Munteanu on 3/25/19.
+//  Copyright © 2019 Vlad Munteanu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
